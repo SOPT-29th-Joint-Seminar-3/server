29bf008c4851a3f1d009abe2c13035d251f4774e
LeeJejune <leejejune0625@gmail.com>
2021-11-20 15:14:03 +0900

    [add].
    .

---
+++ functions/index.js
@@ -0,0 +1,18 @@
+const admin = require("firebase-admin");
+const serviceAccount = require("./wesopt29-328c5-firebase-adminsdk-lppnh-dae9ebc006.jsonn");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+let firebase;
+if (admin.apps.length === 0) {
+  firebase = admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+} else {
+  firebase = admin.app();
+}
+
+module.exports = {
+  api: require("./api"),
+};
